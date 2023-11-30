b4575ca4b0f6ddc9a5531e0b09456a71ee324e5e
Douglas Figueiredo <douglas.profi@yahoo.com>
2023-11-30 19:41:02 -0300

    Adição de evento teste no botão Follow

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -3,13 +3,20 @@ import Title from "../Title";
 import ProfileSection from "./ProfileSection";
 import styles from "./styles.module.css";
 
+function handleClick(ev) {
+  console.log(ev);
+  alert("Você agora está seguindo!");
+}
+
 export default function Profile(props) {
   return (
     <div className={styles.container}>
       <img className={styles.avatar} src={props.avatar} alt={props.name} />
       <Title>
-        <span>John Doe</span>
-        <button>Follow</button>
+        <span>{props.name}</span>
+        <button className={styles.followButton} onClick={handleClick}>
+          Follow
+        </button>
       </Title>
       <ProfileSection>{props.bio}</ProfileSection>
       <ProfileSection>{props.phone}</ProfileSection>
